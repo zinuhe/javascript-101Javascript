93fd6d490ac200d37317a273ddf2685dda3f3a03
Jimmy <zinuhe@outlook.com>
2023-10-10 21:30:27 -0400

    Add comments

--- Promises/fetch.js
+++ Promises/fetch.js
@@ -4,7 +4,7 @@ const apiUrl = 'http://worldtimeapi.org/api/timezone/America/New_York'
 async function getData() {
   try {
     const response = await fetch(apiUrl)
-    const data = await response.json() // json() it's a function that returns a promise
+    const data = await response.json() // function .json() returns a promise, we can use await to assign the response to a variable.
     console.log(data)
   } catch (error) {
     console.log('Error: ', error)
