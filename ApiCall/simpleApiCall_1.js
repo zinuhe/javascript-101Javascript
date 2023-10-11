6df6e6b66ee0b9e6fd7f48783076133df6564836
Jimmy <zinuhe@outlook.com>
2023-10-10 21:43:42 -0400

    Small changes

--- ApiCall/simpleApiCall_1.js
+++ ApiCall/simpleApiCall_1.js
@@ -2,28 +2,28 @@ console.log("POKEMON - SIMPLE API CALL");
 
 // Calling an API using Fetch
 async function getPokemons() {
-    const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10';
-    const response = await fetch(apiUrl);
-    const result = await response.json();  // function .json() returns a promise, we can use await to assign the response to a variable.
-    const pokemons = result.results;
+  const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10';
+  const response = await fetch(apiUrl);
+  const result = await response.json();  // function .json() returns a promise, we can use await to assign the response to a variable.
+  const pokemons = result.results;
 
-    // console.log(pokemons);
-    return pokemons;
-  }
+  // console.log(pokemons);
+  return pokemons;
+}
 
 // async function will always return a promise and you have to use .then() or await to access its value
 
 // Approach 1: wrapping into an anonymous async function **better.
 // If we’re not using modules, or older browsers must be supported
 (async () => {
-    const response = await getPokemons();
-    console.log('Pokemons fetched(1):', response);
+  const response = await getPokemons();
+  console.log('Pokemons fetched(1):', response);
 })();
 
 // Approach 1*: named function
 (async function () {
-    const response = await getPokemons();
-    console.log('Pokemons fetched(1*):', response);
+  const response = await getPokemons();
+  console.log('Pokemons fetched(1*):', response);
 })();
 
 // Approach 2: .then()
--- ApiCall/simpleApiCall_2.js
+++ ApiCall/simpleApiCall_2.js
@@ -1,25 +1,25 @@
 console.log('POKEMON - GET API CALL')
 
 async function getPokemons() {
-    const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10'
-    const response = await fetch(apiUrl)
-    const result = await response.json() // function .json() returns a promise, we can use await to assign the response to a variable.
+  const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10'
+  const response = await fetch(apiUrl)
+  const result = await response.json() // function .json() returns a promise, we can use await to assign the response to a variable.
 
-    return result.results;
-  }
+  return result.results;
+}
 
 (async () => {
-try {
-  const response = await getPokemons()
-  console.log('Pokemons:', response)
+  try {
+    const response = await getPokemons()
+    console.log('Pokemons:', response)
 
-  // Iterate through the response data
-  response.forEach(pokemon => {
-    console.log('Name:', pokemon.name)
-    console.log('URL:', pokemon.url)
-    console.log('')
-  })
-} catch (error) {
-    console.error('Error: ', error)
-}
+    // Iterate through the response data
+    response.forEach(pokemon => {
+      console.log('Name:', pokemon.name)
+      console.log('URL:', pokemon.url)
+      console.log('')
+    })
+  } catch (error) {
+      console.error('Error: ', error)
+  }
 })();
