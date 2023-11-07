c41fe327c9fb34ad41ab348ccf97daa761b87fa4
Jimmy <zinuhe@outlook.com>
2023-11-06 22:07:55 -0500

    Added routes and others

--- SimpleApi/express/3.express.js
+++ SimpleApi/express/3.express.js
@@ -1,10 +1,11 @@
 // To run this file: npm run dev:5
-
-const express = require ('express') // require -> commonJS
+const express = require('express') // require -> commonJS
+const crypto = require('node:crypto')
+const movies = require('../movies/movies.json')
 
 const app = express()
-const movies = require('../movies/movies.json')
 app.disable('x-powered-by') // disable the header X-Powered-By: Express
+app.use(express.json) // Middleware from Express
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello Express' })
@@ -12,11 +13,49 @@ app.get('/', (req, res) => {
 
 // Movies resources  /movies
 app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      // movie => movie.genre.includes(genre)
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+
   res.json(movies)
 })
 
 app.get('/movies/:id', (req, res) => { // path-to-regexp
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
 
+  res.status(404).json({ message: 'Movie not found.' })
+})
+
+app.post('/movies', (req, res) => {
+  const {
+    title,
+    genre,
+    year,
+    director,
+    duration,
+    rate,
+    poster
+  } = req.body
+
+  const newMovie = {
+    id: crypto.randomUUID(), // creates an UUID v4
+    title,
+    genre,
+    year,
+    director,
+    duration,
+    rate: rate ?? 0,
+    poster
+  }
+  movies.push()
+  res.status(201).json(newMovie) // return the same resource created
 })
 
 const PORT = 1234
